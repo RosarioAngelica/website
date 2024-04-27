1a06b50201c46b2e447cb112ce856a3e0a4bc5e4
Angelica G. Rosario <angelicalykarosario@gmail.com>
2024-04-27 20:03:36 +0800

    add new changes

--- myscript.js
+++ myscript.js
@@ -1,23 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let  form = document.getElementById("commentForm");
-    let nameInput = document.getElementById("name");
-    let commentInput = document.getElementById("comment");
-    let submitButton = document.getElementById("submitButton");
-
-    let checkFormFilled = () => {
-        let isFormFilled = nameInput.value.trim() && commentInput.value.trim();
-        submitButton.disabled = !isFormFilled;
-        if (isFormFilled) {
-            submitButton.style.cursor = "pointer";
-        } else {
-            submitButton.style.cursor = "not-allowed";
-        }
-    };
-
-    nameInput.addEventListener("input", checkFormFilled);
-    commentInput.addEventListener("input", checkFormFilled);
-
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-    });
-});
+function validateInput() {
+    let inputField = document.getElementById("comment", "name");
+    let submitBtn = document.getElementById("submit_button");
+  
+    if (inputField.value.trim().length > 0) {
+      submitBtn.disabled = false;
+      submitBtn.style.color = "dark blue";
+    } else {
+      submitBtn.disabled = true;
+      submitBtn.style.color = "";
+    }
+}
+  
+  
